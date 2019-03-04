6b2c6d663083dad81ed56294a8371cea571da407
DarkMorford <DarkMorford@users.noreply.github.com>
2019-03-04 06:15:24 -0800

    Update script to set path and cpath

--- src/pokemon-overlay.lua
+++ src/pokemon-overlay.lua
@@ -1,20 +1,33 @@
 -- Bengineering Pokemon overlay script
+package.path  = _G.script_path() .. [[../lib/lua/?.lua;]] .. package.path
+package.cpath = _G.script_path() .. [[../lib/?.dll;]] .. package.cpath
+
 local obs = _G.obslua
 
 function _G.script_description()
-  return "Uses Google Docs to automate the Pokemon overlay\n\nWritten by DarkMorford"
+  return "Automates Ben's Pokémon overlay by connecting with Google Docs.\n\nWritten by DarkMorford"
 end
 
 function _G.script_properties()
   local props = obs.obs_properties_create()
 
-  obs.obs_properties_add_text(props, "sheet_id", "Google Sheets ID", obs.OBS_TEXT_DEFAULT)
+  obs.obs_properties_add_text(props, "api_key", "Google API Key", obs.OBS_TEXT_PASSWORD)
+  obs.obs_properties_add_text(props, "spreadsheet", "Google Spreadsheet", obs.OBS_TEXT_DEFAULT)
+  obs.obs_properties_add_int(props, "refresh", "Refresh Interval (seconds)", 1, 60, 1)
 
   return props
 end
 
 function _G.script_defaults(settings)
+  obs.obs_data_set_default_string(settings, "spreadsheet", "1SvOZFM3yPPdQ00bFIK9Og4CwucKHjsFzwKRcRnF-vVg")
+  obs.obs_data_set_default_int(settings, "refresh", 5)
 end
 
 function _G.script_update(settings)
+end
+
+function _G.script_load(settings)
+end
+
+function _G.script_unload()
 end
